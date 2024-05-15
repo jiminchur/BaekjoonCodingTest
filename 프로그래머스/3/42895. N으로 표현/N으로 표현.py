4bc9d3b5e8eba3e1e3b9ac3522c0358e4236e1ee
MinChur <145955453+jiminchur@users.noreply.github.com>
2024-05-15 15:19:34 +0900

    [level 3] Title: N으로 표현, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -0,0 +1,21 @@
+def solution(N, number):
+    if N == number:
+        return 1
+    
+    dp = [set() for _ in range(9)]
+    for i in range(1, 9):
+        dp[i].add(int(str(N) * i))
+    
+    for i in range(1, 9):
+        for j in range(1, i):
+            for num1 in dp[j]:
+                for num2 in dp[i-j]:
+                    dp[i].add(num1 + num2)
+                    dp[i].add(num1 - num2)
+                    dp[i].add(num1 * num2)
+                    if num2 != 0:
+                        dp[i].add(num1 // num2)
+        if number in dp[i]:
+            return i
+    
+    return -1
