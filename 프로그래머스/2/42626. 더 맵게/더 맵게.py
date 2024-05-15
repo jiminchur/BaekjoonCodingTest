d0f3bfd119a9292d291a8d9c7ababf912cd6be09
MinChur <145955453+jiminchur@users.noreply.github.com>
2024-05-15 17:38:06 +0900

    [level 2] Title: 더 맵게, Time: 1654.36 ms, Memory: 51.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,19 @@
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    mix_count = 0
+
+    while True:
+        first_min = heapq.heappop(scoville)
+        
+        if first_min >= K:
+            return mix_count
+        
+        elif len(scoville) == 0:
+            return -1
+        
+        second_min = heapq.heappop(scoville)
+        new_scoville = first_min + (second_min * 2)
+        heapq.heappush(scoville, new_scoville)
+        mix_count += 1
