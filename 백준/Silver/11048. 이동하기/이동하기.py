7f900df64dd13501332e79be03480d08a46903fa
MinChur <145955453+jiminchur@users.noreply.github.com>
2024-12-06 16:24:03 +0900

    [Silver II] Title: 이동하기, Time: 1384 ms, Memory: 78728 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.py
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -3,13 +3,10 @@ import sys
 N, M = map(int, sys.stdin.readline().split())
 maze = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-# DP 테이블 초기화
 dp = [[0] * M for _ in range(N)]
 
-# 시작 지점 초기화
 dp[0][0] = maze[0][0]
 
-# DP 테이블 채우기
 for i in range(N):
     for j in range(M):
         if i > 0:
@@ -19,5 +16,4 @@ for i in range(N):
         if i > 0 and j > 0:
             dp[i][j] = max(dp[i][j], dp[i-1][j-1] + maze[i][j])
 
-# 결과 출력
 print(dp[N-1][M-1])
