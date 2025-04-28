086cc429f58d6d5aca7db2d6d14e6e031f4b0432
MinChur <145955453+jiminchur@users.noreply.github.com>
2025-04-28 22:38:08 +0900

    [Silver V] Title: 영화감독 숌, Time: 612 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+count = 0
+number = 666
+
+while True:
+    if '666' in str(number):
+        count += 1
+        if count == n:
+            print(number)
+            break
+    number += 1
